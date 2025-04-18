80aa58a17182344161df3528ce6230784a2cd86d
Yang <123286626+yshi056@users.noreply.github.com>
2025-04-18 19:01:01 -0400

    Create global.d.ts

---
+++ server/types/global.d.ts
@@ -0,0 +1,7 @@
+// Type declarations for modules without @types packages
+declare module 'jsonwebtoken';
+declare module 'bcrypt';
+declare module 'cors';
+declare module 'swagger-ui-express';
+declare module 'cookie-parser';
+declare module 'express-openapi-validator';
